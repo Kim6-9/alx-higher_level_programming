e87b34fb7c44db964f6c6bd09c97d496c8a792b6
Kim6-9 <you@example.com>
2024-06-20 10:42:26 -0700

    my

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,17 @@
+
+
+
 #!/usr/bin/node
+// prints a square, 1st argument is the size of the square
 const size = Math.floor(Number(process.argv[2]));
-if (isNaN(size)) {
+if (isNaN(size) === true) {
   console.log('Missing size');
 } else {
-  for (let r = 0; r < size; r++) {
-    let row = '';
-    for (let c = 0; c < size; c++) row += 'X';
-    console.log(row);
+  let line = '';
+  for (let i = 0; i < size; i++) {
+    line += 'X';
+  }
+  for (let j = 0; j < size; j++) {
+    console.log(line);
   }
 }
