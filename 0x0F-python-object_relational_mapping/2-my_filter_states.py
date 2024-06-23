3622170205729a1e6e1f1c05a19bc73b8ccc7586
Kim6-9 <you@example.com>
2024-06-23 09:01:13 -0700

    my commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
+"""takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
 """
-Listing all states from the database hbtn_0e_0_usa
-"""
-
-
-def main():
-    """
-    List 'states' table of 'hbtn_0e_0_usa' database in ascending
-    order by id's
-    """
+if __name__ == "__main__":
     import MySQLdb
-    from sys import argv
-
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    cursor = db.cursor()
-
-    query = """SELECT * FROM states WHERE name LIKE '{}' ORDER BY
-    id;""".format(argv[4])
-
-    cursor.execute(query)
-
-    rows = cursor.fetchall()
-    for row in rows:
-        if (row[1] == argv[4]):
-            print(row)
-
-    cursor.close()
-    db.close()
-
-
-if __name__ == '__main__':
-    main()
+    import sys
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8")
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE '%%{userinput}%%' \
+ORDER BY id ASC".format(
+            userinput=sys.argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
