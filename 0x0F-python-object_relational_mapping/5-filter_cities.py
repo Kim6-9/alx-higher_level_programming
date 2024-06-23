c4cebcb247d8bfdfeb364dc32c271b63460df765
Kim6-9 <you@example.com>
2024-06-23 00:19:41 -0700

    my commit

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,35 +1,16 @@
 #!/usr/bin/python3
-
-"""
-    A script that lists all cities in a state from the database hbtn_0e_0_usa
-    Username, password and database name and state are given as user args
-"""
-
-
-import sys
+Your Comments Here
 import MySQLdb
-
-
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         host='localhost',
-                         port=3306)
-
-    cursor = db.cursor()
-
-    sql = """SELECT cities.name
-          FROM states
-          INNER JOIN cities ON states.id = cities.state_id
-          WHERE states.name = %s
-          ORDER BY cities.id ASC"""
-
-    cursor.execute(sql, (sys.argv[4],))
-
-    data = cursor.fetchall()
-
-    print(", ".join([city[0] for city in data]))
-
-    cursor.close()
-    db.close()
+import sys
+if_name="main":
+db MySQLdb.connect(host="localhost", user-sys.argv[1].
+passwd-sys.argv[2], db-sys.argv[3], port=3306)
+cdb.cursor()
+c.execute("SELECT cities.name FROM
+cities INNER JOIN states ON states. id=cities.state_id
+WHERE states.names, (sys.argv[4],))
+rows c.fetchall()
+tap list(row[0] for row in rows)
+print("tmp, sep", ")
+c.close()
+db.close()
