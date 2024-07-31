9309706e41024a68fd4a015afca94a72fd9160bf
Kim6-9 <you@example.com>
2024-07-30 18:13:49 -0700

    my

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.writeFileSync(process.argv[2], process.argv[3]);
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
