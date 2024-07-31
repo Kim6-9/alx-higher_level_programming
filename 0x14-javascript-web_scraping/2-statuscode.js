282457222a20b170cbc8693f51b0934329fc09d0
Kim6-9 <you@example.com>
2024-07-30 18:16:59 -0700

    my

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
+// Status Of Get request
+
 const request = require('request');
-request(process.argv[2], function (error, response) {
-  if (error == null) {
-    console.log('code: ' + response.statusCode);
+const url = process.argv[2];
+request.get(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:' + ' ' + response.statusCode);
   }
 });
